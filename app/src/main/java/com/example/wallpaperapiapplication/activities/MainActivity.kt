11f7761d1d9aceefc8fae31c46a0bc7e98263b83
RafayKakar <rafaykakar1029@gmail.com>
2022-12-15 00:08:27 +0500

    Wallpaper Application

--- app/src/main/java/com/example/wallpaperapiapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperapiapplication/activities/MainActivity.kt
@@ -5,25 +5,17 @@ import android.content.IntentFilter
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.FileObserver
-import android.util.Log
 import android.view.MenuItem
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.example.wallpaperapiapplication.R
 import com.example.wallpaperapiapplication.databinding.ActivityMainBinding
 import com.example.wallpaperapiapplication.fragments.DownloadFragment
 import com.example.wallpaperapiapplication.fragments.WallpaperFragment
 import com.example.wallpaperapiapplication.recievers.NetworkReciever
-import com.example.wallpaperapiapplication.viewmodels.WallpaperViewModel
 import com.example.wallpaperapplication.adapters.ViewPagerAdapter
-import dagger.hilt.android.AndroidEntryPoint
 
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
